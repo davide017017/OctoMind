2796885cfca524b596fb649bcfb0f11adec1ad51
Davide <davide017@hotmail.it>
2026-02-16 20:25:56 +0100

    UI: improved dashboard layout, compact repo cards with multi-language badges

--- OMfrontend/config.js
+++ OMfrontend/config.js
@@ -1,5 +1,5 @@
 // OMfrontend/config.js
 
-export const API_BASE_URL = "http://127.0.0.1:8000";
+// export const API_BASE_URL = "http://127.0.0.1:8000";
 
-// export const API_BASE_URL = "https://octomind.onrender.com";
+export const API_BASE_URL = "https://octomind.onrender.com";
